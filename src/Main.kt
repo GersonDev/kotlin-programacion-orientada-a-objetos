4c669ac71ebf7403dc6cc87bdd7b4565c2f60fe0
Gerson <gersoncornelio1@gmail.com>
2022-03-29 10:54:34 -0500

    LAVADORA

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,31 @@
-fun main(args:Array<String>){
+fun main(args: Array<String>) {
+    val construc1 = lavadora()
+    var construc2 = lavadora(color = "Negro", modelo = "AOC38D", marca = "AOC")
+    println("El color nuevo es :${construc2.color} y el modelo nuevo es: ${construc2.modelo}" +
+            " y la marca nueva es: ${construc2.marca} ")
 
+}
+class lavadora {
+    var color: String? = null
+    var marca: String? = null
+    var stock: Boolean? = null
+    var precio: Double? = null
+    var modelo: String? = null
+    constructor() {
+        color = "Blanco"
+        marca = "SANSUMG"
+        stock = true
+        precio = 599.90
+        modelo = "SANSUMG98DS"
+    }
+
+    constructor(color: String, marca: String, modelo: String) {
+        this.color = color
+        this.marca = marca
+        this.modelo = modelo
+    }
+
+    fun potencia(): Unit {
+        println("EN LA POTENCIA DE LA AOC ES : 400HP")
+    }
 }
