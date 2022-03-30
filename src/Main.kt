643f97cf795a790e598c9c169a7e8d4b11014ede
Gerson <gersoncornelio1@gmail.com>
2022-03-30 08:05:14 -0500

    se creo un ejercicio de calculadora

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,40 @@
 fun main(args: Array<String>) {
     val construc1 = calculadora()
     var construc2 = calculadora(color = "AZUL", modelo = "Bale372", marca = "CLOCK", precio = 699.99)
-    println("El color nuevo es :${construc2.color} y el modelo nuevo es: ${construc2.modelo}" +
-            " y la marca nueva es: ${construc2.marca} y el precio es:${construc2.precio}")
+    println(
+        "El color nuevo es :${construc2.color} y el modelo nuevo es: ${construc2.modelo}" +
+                " y la marca nueva es: ${construc2.marca} y el precio es:${construc2.precio}"
+    )
+    println("PRIMER NUMERO")
+    var primerNumero = readLine()!!.toInt()
+    println("SEGUNDO NUMERO")
+    var segundoNumero = readLine()!!.toInt()
+    val suma = sumar(primerNumero, segundoNumero)
+    val resta = restar(primerNumero, segundoNumero)
+    val multiplicar = multiplicar(primerNumero, segundoNumero)
+    val dividir = dividir(primerNumero, segundoNumero)
+    println("La suma es $suma")
+    println("La resta es $resta")
+    println("La multiplicacion es $multiplicar")
+    println("La division es $dividir")
 }
+
+fun sumar(primerNumero: Int, segundoNumero: Int): Int {
+    return primerNumero + segundoNumero
+}
+
+fun restar(primerNumero: Int, segundoNumero: Int): Int {
+    return primerNumero - segundoNumero
+}
+
+fun multiplicar(primerNumero: Int, segundoNumero: Int): Int {
+    return primerNumero * segundoNumero
+}
+
+fun dividir(primerNumero: Int, segundoNumero: Int): Int {
+    return primerNumero / segundoNumero
+}
+
 class calculadora {
     var color: String? = null
     var marca: String? = null
@@ -18,13 +49,11 @@ class calculadora {
         precio = 599.90
         modelo = "CAS9398"
     }
+
     constructor(color: String, marca: String, modelo: String, precio: Double) {
         this.color = color
         this.marca = marca
         this.modelo = modelo
-        this.modelo = modelo
-    }
-    fun potencia(): Unit {
-        println("EN LA POTENCIA DE LA AOC ES : 400HP")
+        this.precio = precio
     }
 }
