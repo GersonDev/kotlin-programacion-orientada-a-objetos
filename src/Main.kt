219a51c5c729bc48f9ab365e1e61ef1251db8543
Gerson <gersoncornelio1@gmail.com>
2022-03-30 16:37:18 -0500

    INGRESE NUEVO NOMBRE Y APELLIDO

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,20 @@
-fun main(args:Array<String>){
+fun main(args: Array<String>) {
+    val c1 = nombre()
+    println("EL NOMBRE ES ${c1.nombre} Y EL APELLIDO ES ${c1.apellido}")
+    println("INGRESE NUEVO NOMBRE Y APELLIDO")
+    val nom = readLine()!!.toString()
+    val apel = readLine()!!.toString()
+    val c2 = nombre(nombre = "$nom", apellido = "$apel")
+    println("EL NUEVO NOMBRE ES ${c2.nombre} y EL NUEVO APELLIDO ES ${c2.apellido}")
+}
 
+class nombre {
+    var nombre: String = "GERSON"
+    var apellido: String = "CORNELIO"
+
+    constructor()
+    constructor(nombre: String, apellido: String) {
+        this.nombre = nombre
+        this.apellido = apellido
+    }
 }
