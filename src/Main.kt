3015d03eb8be623f9d6597184d237cfdc50bcbe3
Gerson <gersoncornelio1@gmail.com>
2022-03-30 08:25:44 -0500

    ventilador

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,28 @@
-fun main(args:Array<String>){
+fun main(args: Array<String>) {
+    val c1 = ventilador()
+    val c2 = ventilador(tipo = "De piso", velocidades = 6, precio = 259.00, marca = "HELIX")
+    println("LA MARCA NUEVA ES: ${c2.marca},el tipo de ventilador es: ${c2.tipo},es de ${c2.velocidades}velocidades,el precio es de ${c2.precio} ")
+    println(caracteristicar())
+}
 
+class ventilador {
+    var tipo: String = "Torre"
+    var velocidades: Int = 5
+    var marca: String = "AOC"
+    val stock: Boolean = true
+    var precio: Double = 199.90
+
+    constructor()
+    constructor(tipo: String, velocidades: Int, precio: Double, marca: String) {
+        this.tipo = tipo
+        this.velocidades = velocidades
+        this.precio = precio
+        this.marca = marca
+
+    }
+
+}
+
+fun caracteristicar() {
+    println("POTENCIA DE 100 HP")
 }
