d5c706447a4bf03d0a0b862b6a3db2f40d47b981
Gerson <gersoncornelio1@gmail.com>
2022-03-30 10:36:02 -0500

    CIRCULO

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,30 @@
-fun main(args:Array<String>){
+fun main(args: Array<String>) {
+    val c1 = circulo()
+    println("EL RADIO DEL CIRCULO ES ${c1.calcularRadio()} y el  area es ${c1.calcularArea()}")
+    val c2 = circulo(2.0)
+    println("EL RADIO DEL CIRCULO ES ${c2.calcularRadio()} y el area es ${c2.calcularArea()}")
+}
 
+class circulo {
+    var radio: Double? = null
+    var color: String? = null
+
+    constructor() {
+        radio = 1.0
+        color = "rojo"
+
+    }
+
+    constructor(r: Double) {
+        radio = r
+        color = "rojo"
+    }
+
+    fun calcularRadio(): Double? {
+        return radio
+    }
+
+    fun calcularArea(): Double? {
+        return (radio ?: 0.0) * (radio ?: 0.0) * Math.PI
+    }
 }
