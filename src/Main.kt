6c6b4bd24b64f33daefeb4b4798a504a82e46fa1
Gerson <gersoncornelio1@gmail.com>
2022-03-31 08:57:48 -0500

    INGRESE NUEVO NOMBRE

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,12 @@
 fun main(args:Array<String>){
-
+    println("EL NOMBRE ES ${nombreDetereminado()}")
+    println("INGRESE NUEVO NOMBRE Y APELLIDO")
+    val nom = readLine()!!.toString()
+    println("EL NUEVO NOMBRE ES: $nom")
+}
+class Nombre{
+constructor()
+}
+public fun nombreDetereminado():Unit{
+    println("MOHAMEND  FAISAL")
 }
