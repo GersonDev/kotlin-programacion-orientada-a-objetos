2dad78a2ae7dcc5a763473f9c18a3be48ba41577
Gerson <gersoncornelio1@gmail.com>
2022-03-31 22:47:08 -0500

    CONVERSION DE F" A C"

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
-fun main(args:Array<String>){
+fun main(args: Array<String>) {
+    println("INGRESE GRADOS FAHRENHEIT:")
+    var farent: Double = readLine()!!.toDouble()
+    val c1 = Q2(farent)
+    println("LA CONVERSION DE $farent FARENHEIT a GRADOS CELSIUS ES :${c1.calculuarConversion()}")
+}
 
+class Q2 {
+    var farent: Double? = null
+
+    constructor(farent: Double) {
+        this.farent = farent
+    }
+
+    fun calculuarConversion(): Double? {
+        return ((farent ?: 0.0) - 32.0) * (5.0 / 9.0)
+    }
 }
