c15ce23f765befb556ac765d55452d33c0e65f0b
Gerson <gersoncornelio1@gmail.com>
2022-04-04 09:57:20 -0500

    OPERACION MATEMATICAS DE 2 NUMEROS

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,22 @@
-fun main(args:Array<String>){
-
+fun main(args: Array<String>) {
+    println("INGRESE EL PRIMER NUMERO:")
+    var num1 = readLine()!!.toInt()
+    println("INGRESE EL SEGUNDO NUMERO:")
+    var num2 = readLine()!!.toInt()
+    var c1 = Operaciones()
+    println(c1.calculos(num1, num2))
+}
+class Operaciones {
+    var num1: Int? = null
+    var num2: Int? = null
+    constructor() {
+        num1 = num1
+        num1 = num1
+    }
+    fun calculos(num1: Int, num2: Int) {
+        println("La suma es:${num1 + num2}")
+        println("La diferencia es:${num1 - num2}")
+        println("el producto es:${num1 * num2}")
+        println("el cociente es:${num1 / num2}")
+    }
 }
