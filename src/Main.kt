a5604bd972a1226bdeda153e6ceca536a2487e54
Gerson <gersoncornelio1@gmail.com>
2022-04-04 16:20:32 -0500

    MAYOR MENOR O IGUAL DE 2 NUMEROS

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,22 @@
-fun main(args:Array<String>){
-
+fun main(args: Array<String>) {
+    println("PRIMER NUMERO")
+    var num1 = readLine()!!.toInt()
+    println("SEGUNDO NUMERO")
+    var num2 = readLine()!!.toInt()
+    val c1 = Operaciones(num1, num2)
+    println("${c1.requisitos()}")
 }
+class Operaciones(var num1: Int = 0, var num2: Int = 0) {
+    fun requisitos() {
+        if (num1 > num2)
+            println("El Numero Mayor es: $num1")
+        else if (num1 < num2)
+            println("El Numero Mayor es: $num2")
+        else {
+            println("$num1 y $num2 son Igual")
+        }
+    }
+}
+
+
+
