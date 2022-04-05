d5b4b06e3642c88cf46afe8e0e3439723d3a816f
Gerson <gersoncornelio1@gmail.com>
2022-04-05 10:32:16 -0500

    CLASE CALCULAR EL MAYOR DE 10 NUMEROS

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
-fun main(args:Array<String>){
-
+fun main(args: Array<String>) {
+    val c1 = Mayor10()
+    println("${c1.calcularMayor()}")
+}
+class Mayor10(var x: Int = 0, var n: Int = 0, var nummayor: Int = 0) {
+    fun calcularMayor() {
+        for (x in 0..10) {
+            println("Ingrese NUMERO:")
+            n = readLine()!!.toInt()
+            if (nummayor < n)
+                nummayor = n
+        }
+        println("EL MAYOR NUMERO ES $nummayor")
+    }
 }
