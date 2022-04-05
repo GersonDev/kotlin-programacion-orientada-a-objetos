cc62736dcdd852c91749a3f559cbf2a605274c37
Gerson <gersoncornelio1@gmail.com>
2022-04-05 11:07:10 -0500

    CLASE DEL 1AL5 *10*100*1000

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,11 @@
-fun main(args:Array<String>){
+fun main(args: Array<String>) {
+    val c1 = Operacion1al5()
+    println("${c1.multi()}")
+}
 
+class Operacion1al5(var x: Int = 0) {
+    fun multi() {
+        for (x in 1..4)
+            println("$x ${x * 10}  ${x * 100}   ${x * 1000}")
+    }
 }
