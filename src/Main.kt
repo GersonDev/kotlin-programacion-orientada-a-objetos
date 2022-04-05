860cb0d5ae2f0ce4623645020b33f9f6df267a88
Gerson <gersoncornelio1@gmail.com>
2022-04-04 21:46:28 -0500

    MAYOR MENOR SUMA PRODUCTO MEDIA DE 3 NUMEROS

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,18 @@
 fun main(args: Array<String>) {
     println("1ER NUMERO:")
-    var num1 = readLine()!!.toInt()
+    val num1 = readLine()!!.toInt()
     println("2DO NUMERO:")
-    var num2 = readLine()!!.toInt()
+    val num2 = readLine()!!.toInt()
     println("3ER NUMERO:")
-    var num3 = readLine()!!.toInt()
+    val num3 = readLine()!!.toInt()
     val c1 = TresOperaciones(num1, num2, num3)
-    println("${c1.sumaMediaProductoMenor()}")
+    println("${c1.mayorYmenor()}")
+    println("${c1.suma()}")
+    println("${c1.producto()}")
+    println("${c1.media()}")
 }
 class TresOperaciones(var num1: Int, var num2: Int, var num3: Int) {
-    fun sumaMediaProductoMenor() {
+    fun mayorYmenor() {
         if (num1 < num2 && num1 < num3)
             println("El menor numero es: $num1")
         else
@@ -25,9 +28,14 @@ class TresOperaciones(var num1: Int, var num2: Int, var num3: Int) {
                 println("El mayor numero es: $num2")
             else
                 if (num3 > num1 && num3 > num2)
-                    println("El mayor numero es:$num3")
-        println("LA SUMA DE  $num1,$num2,$num3 ES:${num1 + num2 + num3}")
-        println("EL PRODUCTO DE $num1,$num2,$num3 ES:${num1 * num2 * num3}")
-        println("LA MEDIA DE $num1,$num2,$num3 ES: ${(num1 + num2 + num3) / 3}")
+                    println("El mayor numero es:$num3")}
+        fun suma() {
+            println("LA SUMA DE  $num1,$num2,$num3 ES:${num1 + num2 + num3}")
+        }
+        fun producto(){
+            println("EL PRODUCTO DE $num1,$num2,$num3 ES:${num1 * num2 * num3}")
+        }
+        fun media() {
+            println("LA MEDIA DE $num1,$num2,$num3 ES: ${(num1 + num2 + num3) / 3}")
+        }
     }
-}
