1b4dbbdbdd3c7cbbf75692e92e62a6b090579e5c
Gerson <gersoncornelio1@gmail.com>
2022-04-05 11:39:40 -0500

    CLASE DEL RANGO DE DOS NUMEROS Y SUS RESPETIVAS MULTIPLICACION *10*100*1000

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,14 @@
-fun main(args:Array<String>){
+fun main(args: Array<String>) {
+    println("INGRESE EL RANGO DE BUSQUEDA DONDE COMIENZA Y DONDE TERMINA")
+    val num1 = readLine()!!.toInt()
+    val num2 = readLine()!!.toInt()
+    val c1 = Operacion1al5(num1 = num1, num2 = num2)
+    println("${c1.multi()}")
+}
 
+class Operacion1al5(var x: Int = 0, var num1: Int = 0, var num2: Int = 0) {
+    fun multi() {
+        for (x in num1..num2)
+            println("$x ${x * 10}  ${x * 100}   ${x * 1000}")
+    }
 }
