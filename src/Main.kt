3d6f9801e33d445dff38208d4a92412dc2375508
Gerson <gersoncornelio1@gmail.com>
2022-04-05 23:50:48 -0500

    CLASE ESFERA ENCONTRA EL VOLUMEN INGRESANDO EL RADIO

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,14 @@
-fun main(args:Array<String>){
-
+fun main(args: Array<String>) {
+    println("INGRESE RADIO:")
+    val radio = readln()!!.toDouble()
+    val c1 = Esfera(radio = radio)
+    println("${c1.calcularVolumen()}")
 }
+
+class Esfera(var radio: Double = 0.0, var volumen: Double = 0.0) {
+    fun calcularVolumen() {
+        volumen = ((4.0 / 3.0) * Math.PI * Math.pow(radio, 3.0))
+        println("EL VOLUMEN DE LA ESFERA ES: $volumen m3")
+    }
+}
+
