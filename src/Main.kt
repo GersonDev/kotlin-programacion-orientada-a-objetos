157040793f59d320bb16ac27fe8c62eff4d7db9e
Gerson <gersoncornelio1@gmail.com>
2022-04-06 00:24:15 -0500

    CLASES CON ARRAY

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,10 @@
 fun main(args:Array<String>){
-
+val c1=Operacion()
+    println("${c1.array10()}")
+}
+class Operacion() {
+    fun array10() {
+       var arreglo:Array<Int> = Array(10){0}
+        println("$arreglo")
+    }
 }
