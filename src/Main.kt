2f17652ac1d3b4c2f61b88ecd69ad64989d4aa2f
Gerson <gersoncornelio1@gmail.com>
2022-04-06 00:48:12 -0500

    VOLUMEN DE UNA ESFERA

--- src/Main.kt
+++ src/Main.kt
@@ -5,7 +5,8 @@ fun main(args: Array<String>) {
     println("${c1.calcularVolumen()}")
 }
 
-class Esfera(var radio: Double = 0.0, var volumen: Double = 0.0) {
+class Esfera(var radio: Double = 0.0 ) {
+    private var volumen: Double = 0.0
     fun calcularVolumen() {
         volumen = ((4.0 / 3.0) * Math.PI * Math.pow(radio, 3.0))
         println("EL VOLUMEN DE LA ESFERA ES: $volumen m3")
