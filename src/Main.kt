e999e97e8dd4b442ed7d9722e55d08dc95a8fcbc
Gerson <gersoncornelio1@gmail.com>
2022-04-07 09:59:31 -0500

    mayuscula y minuscula de una cadena de string

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
 fun main(args:Array<String>){
-
+println("INGRESE PALABRA QUE SERA TRANSFORMADO A MAYUSCULA Y MINUSCULA")
+    val num1= readLine()!!.toString()
+    val c1=Converter(num1=num1)
+    println("${c1.mayus()}")
+    println("${c1.minus()}")
+}
+class Converter(var num1:String){
+fun mayus(){
+    println("${num1.toUpperCase()}")
+}
+    fun minus(){
+        println("${num1.toLowerCase()}")
+    }
 }
