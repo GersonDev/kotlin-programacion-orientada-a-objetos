fd438325dcc7bced11d30e5adbcf737241683a10
Carlos Vargas Huaman <carlos.vargas.huaman@avantica.net>
2022-04-07 22:44:49 -0500

    Mejora del codigo

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,21 @@
-fun main(args:Array<String>){
-val c1=Operacion()
+fun main(args: Array<String>) {
+    val c1 = Operacion()
     println("${c1.array10()}")
     println("${c1.bonus15()}")
 }
-class Operacion() {
-    private var arreglo:Array<Int> = Array(10){0}
-    private var bonus:Array<Int> = arrayOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+
+class Operacion {
+    private var arreglo: Array<Int> = Array(10) { 0 }
+    private var bonus: Array<Int> = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
+
     fun array10() {
-      arreglo.forEach {
-          println(it)
-      }
+        arreglo.forEach {
+            println(it)
+        }
     }
-    fun bonus15(){
-        for (i in bonus){
+
+    fun bonus15() {
+        for (i in bonus) {
             println(i)
         }
     }
