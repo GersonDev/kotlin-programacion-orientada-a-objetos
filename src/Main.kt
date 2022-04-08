84d610d8f8d2cc66d025b40dc87cf94ec0864854
Gerson <gersoncornelio1@gmail.com>
2022-04-07 21:37:29 -0500

    clase:bucle de 20 numeros con detener en -1 y sacar el promedio

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,21 @@
-fun main(args:Array<String>){
-
+fun main(args: Array<String>) {
+    val c1 = Question()
+    println("${c1.veinte()}")
+    println("${c1.promedio()}")
+}
+class Question(private var i: Int = 0,private var n: Int = 0,private var suma: Int = 0) {
+    fun veinte() {
+        for (i in 1..20) {
+            println("INGRESE $i NUMERO :")
+            n = readLine()!!.toInt()
+            suma += n
+            if (n == -1) {
+                println("SE DETUVO EL BUCLE")
+                break
+            }
+        }
+    }
+    fun promedio() {
+        println("EL PROMEDIO ES ${(suma) / 20}")
+    }
 }
