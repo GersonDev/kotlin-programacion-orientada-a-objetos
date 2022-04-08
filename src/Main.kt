2758943578b826598ee49ec668875be843d7127f
Carlos Vargas Huaman <carlos.vargas.huaman@avantica.net>
2022-04-07 22:48:04 -0500

    Mejora del codigo

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,14 @@
 fun main(args: Array<String>) {
     println("INGRESE RADIO:")
-    val radio = readln()!!.toDouble()
-    val c1 = Esfera(radio = radio)
-    println("${c1.calcularVolumen()}")
+    val radio = readLine()!!.toDouble()
+    val esfera = Esfera(radio = radio)
+    println("${esfera.calcularVolumen()}")
 }
 
-class Esfera(var radio: Double = 0.0 ) {
+class Esfera(private var radio: Double = 0.0) {
+
     private var volumen: Double = 0.0
+
     fun calcularVolumen() {
         volumen = ((4.0 / 3.0) * Math.PI * Math.pow(radio, 3.0))
         println("EL VOLUMEN DE LA ESFERA ES: $volumen m3")
