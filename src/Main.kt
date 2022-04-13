ef134bdb935a2bd01de6b3d4a0a58e31de0b0a1f
Gerson <gersoncornelio1@gmail.com>
2022-04-12 23:54:45 -0500

    restaurante mejorado

--- src/Main.kt
+++ src/Main.kt
@@ -4,7 +4,7 @@ fun main(args: Array<String>) {
 
     var opcionDeLaCartaPrincipal = 0
     var opcionDeLaComidaSeleccionada = 0
-    var opcionMedioDePagoSeleccionado=0
+    var opcionMedioDePagoSeleccionado = 0
 
 
     var ejecutarMenuPrincipal = true
@@ -33,38 +33,40 @@ fun main(args: Array<String>) {
                 repositoriosDeMedioDePago.obtenerMediosDePago().forEach {
                     println("${it.id}.${it.nombre}")
                 }
-                opcionMedioDePagoSeleccionado= readLine()!!.toInt()
+                opcionMedioDePagoSeleccionado = readLine()!!.toInt()
             }
-            VER_RESUMEN_DE_COMPRA->{
-                val comidaEncontrada=repositoriosDeComidas.obtenerComidaPorId(opcionDeLaComidaSeleccionada)
+            VER_RESUMEN_DE_COMPRA -> {
+                val comidaEncontrada = repositoriosDeComidas.obtenerComidaPorId(opcionDeLaComidaSeleccionada)
                 println("Su comida es ${comidaEncontrada.nombre}")
-                val medioDepagoSeleccionado=repositoriosDeMedioDePago.obtenerMediosDePagoPorId(opcionMedioDePagoSeleccionado)
+                val medioDepagoSeleccionado =
+                    repositoriosDeMedioDePago.obtenerMediosDePagoPorId(opcionMedioDePagoSeleccionado)
                 println("Su medio de pago es ${medioDepagoSeleccionado.nombre}")
-                ejecutarMenuPrincipal=false
+                ejecutarMenuPrincipal = false
             }
-            SALIR->{
-                ejecutarMenuPrincipal=false
+            SALIR -> {
+                ejecutarMenuPrincipal = false
             }
         }
     }
 }
-const val VER_PLATOSALACARTA=1
-const val REALIZAR_PAGO=2
-const val VER_RESUMEN_DE_COMPRA=3
-const val SALIR=4
+
+const val VER_PLATOSALACARTA = 1
+const val REALIZAR_PAGO = 2
+const val VER_RESUMEN_DE_COMPRA = 3
+const val SALIR = 4
 
 class RepositoriosDeComidas {
     private val comidas: Array<Comida> = arrayOf(
-        Comida(1, "Aji de Gallina",   "Criollo", 20.00),
-        Comida(2, "Cau-Cau",   "Criollo", 15.00),
-        Comida(3, "Ceviche",   "Marino", 30.00),
-        Comida(4, "Chaufa",   "Fusion Peruano-Asiatico", 16.00),
-        Comida(5, "Arroz con Mariscos",   "Marino", 18.00),
-        Comida(6, "Anticucho",   "Criollo", 25.00),
-        Comida(7, "Sopa wantan",   "Fusion Peruano-Asiatico", 12.00),
-        Comida(8, "Carapulcra",   "Criollo", 19.00),
-        Comida(9, "Chaufa de Mariscos",   "Fusion Marino-asiatico", 28.00),
-        Comida(10, "Papa a la huancaina",   "Criollo", 8.00)
+        Comida(1, "Aji de Gallina", "Criollo", 20.00),
+        Comida(2, "Cau-Cau", "Criollo", 15.00),
+        Comida(3, "Ceviche", "Marino", 30.00),
+        Comida(4, "Chaufa", "Fusion Peruano-Asiatico", 16.00),
+        Comida(5, "Arroz con Mariscos", "Marino", 18.00),
+        Comida(6, "Anticucho", "Criollo", 25.00),
+        Comida(7, "Sopa wantan", "Fusion Peruano-Asiatico", 12.00),
+        Comida(8, "Carapulcra", "Criollo", 19.00),
+        Comida(9, "Chaufa de Mariscos", "Fusion Marino-asiatico", 28.00),
+        Comida(10, "Papa a la huancaina", "Criollo", 8.00)
     )
 
     fun obtenerTodasLasComidas(): Array<Comida> {
