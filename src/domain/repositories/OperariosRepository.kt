3d7943b0ee155fe2b64631cc2188b88789311eb0
Gerson <gersoncornelio1@gmail.com>
2022-05-23 12:04:15 -0500

    Crear OperaciosRepository que es de tipo lista mutable con init como inicializador

--- src/domain/repositories/OperariosRepository.kt
+++ src/domain/repositories/OperariosRepository.kt
@@ -4,17 +4,14 @@ import domain.models.Operario
 import java.util.Stack
 
 class OperariosRepository {
- private val operarios:Stack<Operario> =Stack()
- init {
-     operarios.push(Operario(1,90))
-     operarios.push(Operario(2,111))
-     operarios.push(Operario(3,40))
- }
-    fun popTheFirstOperario(): Operario? {
-        return operarios.pop()
-    }
+ private val operarios:MutableList<Operario> = mutableListOf()
+init {
+    operarios.add(Operario(1, 90))
+    operarios.add(Operario(2, 111))
+    operarios.add(Operario(3, 40))
+}
 
-    fun imprimirOperarios() {
-        println(operarios.toString())
+    fun obtenerOperacios():MutableList<Operario>{
+        return operarios
     }
 }
