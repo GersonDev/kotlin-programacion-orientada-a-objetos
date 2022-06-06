8fbbc563d12913419f830bb67d85457fe0fb606c
Gerson <gersoncornelio1@gmail.com>
2022-06-06 13:56:40 -0500

    ejemplos de herencia

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,21 @@
-fun main(args:Array<String>){
-
+fun main(args: Array<String>) {
+    val pt = Point(2.0, 6.5)
+    pt.display()
+    pt.move(-2.0,3.25)
+    pt.display()
 }
+class Point(var x: Double, var y: Double) {
+    fun move(mx: Double, my: Double) {
+
+        x += mx;
+
+        y += my;
+
+    }
+    fun display() {
+
+        print("x : $x  y : $y");
+    }
+}
+
+
