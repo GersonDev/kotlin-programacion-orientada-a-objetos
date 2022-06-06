e2b028c77a9b905e0ee39a995f465dcf0f87be9a
Gerson <gersoncornelio1@gmail.com>
2022-06-06 11:07:41 -0500

    ejemplos de herencia

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
-fun main(args:Array<String>){
-
+fun main(args: Array<String>) {
+    val  bicicleta=Bicicleta()
+        bicicleta.iniciar()
+}
+open class Vehiculo(){
+   open fun iniciar(){
+        println("El vehiculo se ha iniciado")
+    }
+}
+class Bicicleta():Vehiculo(){
+    override fun iniciar(){
+        println("la bicicleta se ha iniciado")
+        super.iniciar()
+    }
 }
